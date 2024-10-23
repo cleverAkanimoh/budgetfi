8039e645a2d2b0bb85dc7cc0ca0cd8517df4061e
cleverakanimoh <cleverakanimoh02@gmail.com>
2024-10-23 12:27:44 +0100

    modified _layout.tsx in (tabs)

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,30 +1,56 @@
 import { Tabs } from "expo-router";
 import React from "react";
-import { SimpleLineIcons } from "@expo/vector-icons";
+import { AntDesign, FontAwesome, SimpleLineIcons } from "@expo/vector-icons";
 import { View } from "react-native";
 import Colors from "@/constants/Colors";
 
 const Layout = () => {
   return (
     <Tabs>
-      <Tabs.Screen
+      <TabsNavigation
         name="index"
-        options={{
-          tabBarIcon: ({ color, size, focused }) => (
-            <View
-              style={{
-                padding: 12,
-                borderRadius: 30,
-                backgroundColor: focused ? Colors.tintColor : Colors.grey,
-              }}
-            >
-              <SimpleLineIcons name="pie-chart" size={18} color={color} />
-            </View>
-          ),
-        }}
+        NavigationIcon={SimpleLineIcons}
+        iconName="pie-chart"
+      />
+      <TabsNavigation
+        name="transactions"
+        NavigationIcon={AntDesign}
+        iconName="swap"
+      />
+      <TabsNavigation
+        name="profile"
+        NavigationIcon={FontAwesome}
+        iconName="user-0"
       />
     </Tabs>
   );
 };
 
 export default Layout;
+
+const TabsNavigation = ({
+  name,
+  NavigationIcon,
+  iconName,
+}: {
+  name: string;
+  iconName: string;
+  NavigationIcon: React.ElementType;
+}) => (
+  <Tabs.Screen
+    name={name}
+    options={{
+      tabBarIcon: ({ color, size, focused }) => (
+        <View
+          style={{
+            padding: 12,
+            borderRadius: 30,
+            backgroundColor: focused ? Colors.tintColor : Colors.grey,
+          }}
+        >
+          <NavigationIcon name={iconName} size={18} color={color} />
+        </View>
+      ),
+    }}
+  />
+);
