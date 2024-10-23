b5534e7e4d87b331e9121addbaa00e7bb492c9af
cleverakanimoh <cleverakanimoh02@gmail.com>
2024-10-23 11:25:45 +0100

    added 16 files

---
+++ constants/icons.ts
@@ -0,0 +1,23 @@
+import DollarSignIcon from "@/assets/icons/dollar-sign.png";
+import AirbnbIcon from "@/assets/icons/airbnb.svg";
+import WalletAddMoneyIcon from "@/assets/icons/money-wallet.png";
+import WalletCardIcon from "@/assets/icons/wallet-credit-card.png";
+import AmazonIcon from "@/assets/icons/amazon.svg";
+import UberIcon from "@/assets/icons/uber-logo.svg";
+import FigmaIcon from "@/assets/icons/figma.svg";
+import ShoppingCartIcon from "@/assets/icons/shopping-cart.png";
+import SpotifyIcon from "@/assets/icons/spotify-logo-light.svg";
+import NetflixIcon from "@/assets/icons/netflix-logo.svg";
+
+export {
+  DollarSignIcon,
+  AirbnbIcon,
+  WalletAddMoneyIcon,
+  WalletCardIcon,
+  AmazonIcon,
+  UberIcon,
+  FigmaIcon,
+  ShoppingCartIcon,
+  SpotifyIcon,
+  NetflixIcon,
+};
