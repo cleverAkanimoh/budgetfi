df2ef0696d0a3409465aa6bda35a42dd6e5f9c6c
cleverakanimoh <cleverakanimoh02@gmail.com>
2024-10-23 11:25:52 +0100

    changed 32 files

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const Page = () => {
+const profile = () => {
   return (
     <View>
-      <Text>Page</Text>
+      <Text>profile</Text>
     </View>
   );
 };
 
-export default Page;
+export default profile;
