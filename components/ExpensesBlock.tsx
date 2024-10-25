f58ca2c3f649c66668fd38bc97ffdc133dc664e4
cleverakanimoh <cleverakanimoh02@gmail.com>
2024-10-25 09:51:07 +0100

    modified ExpensesBlock.tsx in components

--- components/ExpensesBlock.tsx
+++ components/ExpensesBlock.tsx
@@ -1,24 +1,91 @@
-import { FlatList, ListRenderItem, StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  ListRenderItem,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
 import { ExpenseType } from "@/types";
 import Colors from "@/constants/Colors";
+import { Feather } from "@expo/vector-icons";
 
 const ExpensesBlock = ({ expenseList }: { expenseList: ExpenseType[] }) => {
-  const renderItem: ListRenderItem<Partial<ExpenseType>> = ({ item }) => {
+  const renderItem: ListRenderItem<Partial<ExpenseType>> = ({
+    item,
+    index,
+  }) => {
+    if (index === 0) {
+      return (
+        <TouchableOpacity onPress={() => {}}>
+          <View style={styles.addExpenses}>
+            <Feather name="plus" size={22} color={"#ccc"} />
+          </View>
+        </TouchableOpacity>
+      );
+    }
     const amount = item.amount?.split(".");
     const amountPrefix = amount?.[0];
     const amountSuffix = amount?.[1];
     return (
-      <View style={[styles.expensesBlock, { backgroundColor: "" }]}>
-        <Text style={[styles.text, { fontSize: 12 }]}>{item.name}</Text>
-        <Text style={[styles.text, { fontSize: 16, fontWeight: "600" }]}>
+      <View
+        style={[
+          styles.expensesBlock,
+          {
+            backgroundColor:
+              item.id === "2"
+                ? Colors.blue
+                : item.id === "3"
+                ? Colors.white
+                : Colors.tintColor,
+          },
+        ]}
+      >
+        <Text
+          style={[
+            {
+              fontSize: 12,
+              color:
+                item.id === "2"
+                  ? Colors.black
+                  : item.id === "3"
+                  ? Colors.black
+                  : Colors.white,
+            },
+          ]}
+        >
+          {item.name}
+        </Text>
+        <Text
+          style={{
+            fontSize: 16,
+            fontWeight: "600",
+            color:
+              item.id === "2"
+                ? Colors.black
+                : item.id === "3"
+                ? Colors.black
+                : Colors.white,
+          }}
+        >
           ${amountPrefix}.
           <Text style={{ fontWeight: "400", fontSize: 12 }}>
             {amountSuffix}
           </Text>
         </Text>
         <View style={styles.expensePercentage}>
-          <Text style={[styles.text, { fontSize: 12 }]}>
+          <Text
+            style={{
+              fontSize: 12,
+              color:
+                item.id === "2"
+                  ? Colors.black
+                  : item.id === "3"
+                  ? Colors.black
+                  : Colors.white,
+            }}
+          >
             {item.percentage}%
           </Text>
         </View>
@@ -26,10 +93,12 @@ const ExpensesBlock = ({ expenseList }: { expenseList: ExpenseType[] }) => {
     );
   };
 
+  const _static = [{ name: "Add Item" }];
+
   return (
     <View>
       <FlatList
-        data={expenseList}
+        data={_static.concat(expenseList)}
         renderItem={renderItem}
         horizontal
         showsHorizontalScrollIndicator={false}
@@ -58,6 +127,17 @@ const styles = StyleSheet.create({
     backgroundColor: "rgba(255,255,255, 0.2)",
     paddingHorizontal: 5,
     paddingVertical: 3,
+    borderRadius: 8,
+  },
+  addExpenses: {
+    flex: 1,
+    borderWidth: 2,
+    borderColor: "#666",
+    borderStyle: "dashed",
     borderRadius: 10,
+    width: 70,
+    alignItems: "center",
+    justifyContent: "center",
+    marginRight: 20,
   },
 });
